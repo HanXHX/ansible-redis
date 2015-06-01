ec619d23792446d7ba7ce10816f77d1bc82645ee
Emilien Mantel <emilien.mantel@thenextpulse.com>
2015-06-01 11:28:53 +0200

    Remove test running redis on travis

--- tests/test.sh
+++ tests/test.sh
@@ -14,12 +14,6 @@ ansible-playbook -i $INVENTORY_FILE -c local --syntax-check -vv $PLAYBOOK
 # Check role
 ansible-playbook -i $INVENTORY_FILE -c local --sudo -vv $PLAYBOOK
 
-# Check Redis accepts connections
-nc -vz localhost 6379 2>&1 \
-| grep -q 'succeeded' \
-&& (echo 'Redis UP' && exit 0) \
-|| (echo 'Redis DOWN' && exit 1)
-
 # Check indempotence
 ansible-playbook -i $INVENTORY_FILE -c local --sudo -vv $PLAYBOOK \
 | grep -q 'changed=0.*failed=0' \
